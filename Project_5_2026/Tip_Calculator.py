2011cd89d8cd785219d58a2ebc5e8e6cdb4a13a0
egm2982-svg <egm2982@email.vccs.edu>
2026-04-01 14:48:24 -0400

    Class Notes

--- Project_5_2026/Tip_Calculator.py
+++ Project_5_2026/Tip_Calculator.py
@@ -1,1 +1,14 @@
-# Pip install tkinter
+import tkinter as tk #pulling in the extenension tkinter which will generate the userface graphics
+from tkinter import
+def 
+    root = tk.TK() #establishes the tkinter needs to be activated; root is the variable
+# Widgets will now be added - Autocompletion will allow us to pull in the code - Use AI to explain the actions in the library 
+#label = tk.Label(root, text='GeeksForGeeks.org!') label.pack()
+#root,title("") #button=tk.Button(root, text="Stop", width=25, command=root.destroy) button.pack() #root.mainloop #command is what the action is uppon use of the button
+#entry1.grid(row-0, column1) - use to create a command entry #resize must be called in order to move pixels to match window size
+#fixed for resize is an option to avoid any odd auto formats
+#var1=tk.IntVar() #var2=tk.Intvar() -> #tk.checkbutton(root, text='male',variable=var1).grid(row-0, sticky=tk.w) *sticky will move if possible* W signifies west*
+#Checkbutton needs a variable to pull and push information to - txtt with A and B can be used as .pack(anchor) to creat options
+#lb=tk.Listbox(root) #lb.insert(#value)
+#canvas can draw mathematical equations - use for formulas
+root.mainloop() #checking and waiting for input END
